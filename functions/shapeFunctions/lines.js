30d970e7ad5872032c4d7be8e7411c08eae93c19
myl-consultorias <luisarmando.murillobaltodano@gmail.com>
2020-08-01 22:34:55 -0600

    laidLines function in

--- functions/shapeFunctions/lines.js
+++ functions/shapeFunctions/lines.js
@@ -1,10 +1,30 @@
+function getLaidPoints(points) {
+    const pointsToLaid = JSON.parse(JSON.stringify(points));
+    const least = {
+        x: Infinity,
+        y: Infinity
+    };
+
+    pointsToLaid.forEach(({x, y}) => {
+        if (x < least.x) least.x = x;
+        if (y < least.y) least.y = y;
+    });
+
+    pointsToLaid.forEach(vector => {
+        vector.x -= least.x;
+        vector.y -= least.y;
+    });
+
+    return pointsToLaid;
+}
+
 function getRotation(rotation) {
     return rotation / 180 * Math.PI
 }
 
 function getLaidVectorSize(vector) {
     const laidVectorSize = {x: 0, y: 0}
-    if (!vector.laidGroup) vector.laidGroup = vector.group
+    if (!vector.laidGroup) vector.laidGroup = getLaidPoints(vector.group)
     vector.laidGroup.forEach(({x, y}) => {
         if (x > laidVectorSize.x) laidVectorSize.x = x
         if (y > laidVectorSize.y) laidVectorSize.y = y
@@ -18,6 +38,7 @@ function getMidCoord(vector, key) {
 
 export default function lines() {
     // vector {w = 1, c = '#000', group = [{x,y}], x, y}
+    const chosenGroup = this.vector.laidGroup || this.vector.group
     this.ctx.beginPath()
     this.ctx.save()
     if (!isNaN(this.vector.rot) && !this.vector.size) {
@@ -32,12 +53,12 @@ export default function lines() {
         -getMidCoord(this.vector, 'x'),
         -getMidCoord(this.vector, 'y')
     )
-    if (this.vector.group && this.vector.group[0]) {
+    if (chosenGroup && chosenGroup[0]) {
         this.ctx.moveTo(
-            this.vector.group[0].x + (this.vector.x || 0),
-            this.vector.group[0].y + (this.vector.y || 0)
+            chosenGroup[0].x + (this.vector.x || 0),
+            chosenGroup[0].y + (this.vector.y || 0)
         )
-        this.vector.group.forEach((dot, i) => {
+        chosenGroup.forEach((dot, i) => {
             if (i) this.ctx.lineTo(
                 dot.x + (this.vector.x || 0),
                 dot.y + (this.vector.y || 0)
