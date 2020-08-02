48523509e17f9d0b68097401bc8d663e8bc35e82
myl-consultorias <luisarmando.murillobaltodano@gmail.com>
2020-08-01 23:54:01 -0600

    add context to maxFrameRate fix

--- functions/draw.js
+++ functions/draw.js
@@ -1,13 +1,13 @@
 export default function draw(callback, frameRate) {
-    this = this || {}
-    const maxFrameRate = this.maxFrameRate || 60
+    const context = (this || {})
+    const maxFrameRate = context.maxFrameRate || 60
     const rate = frameRate || maxFrameRate
-    const canShowFrame = !(this.currentFrame % Math.floor(maxFrameRate / rate))
-    const currentFrame = this.currentFrame || 0
+    const canShowFrame = !(context.currentFrame % Math.floor(maxFrameRate / rate))
+    const currentFrame = context.currentFrame || 0
 
-    this.currentFrame = (currentFrame + 1) % maxFrameRate
+    context.currentFrame = (currentFrame + 1) % maxFrameRate
 
-    if (canShowFrame) callback(this.currentFrame)
+    if (canShowFrame) callback(context.currentFrame)
 
-    requestAnimationFrame(() => draw.call(this, callback, rate))
+    requestAnimationFrame(() => draw.call(context, callback, rate))
 }
